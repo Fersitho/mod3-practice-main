d4ce81111eb49f2d7f1ea0e43d13e229ca93511a
Fernando Diaz Alaminos <diaz.alaminos.fernando@gmail.com>
2023-10-23 17:36:50 +0200

    exercise 6 finished

--- src/js/exercises/exercise-6.js
+++ src/js/exercises/exercise-6.js
@@ -3,3 +3,23 @@ EJERCICIO 6:
 Genera un array de 10 números aleatorios entre 0 y 100, calcula el menor y
 el mayor y muestra el resultado por pantalla
 */
+const genNumRandom = (totalNumbers) => {
+    const numRandom = [];
+
+    for (let i = 0; i < totalNumbers; i++) {
+        numRandom.push(
+            Math.floor(Math.random() * 100)
+        );
+    }
+
+    const numMin = Math.min(...numRandom);
+    const numMax = Math.max(...numRandom);
+
+    return {
+        arrayOfNumberRandom: numRandom,
+        menor: numMin,
+        mayor: numMax
+    };
+}
+
+showContent(6, genNumRandom(10));
