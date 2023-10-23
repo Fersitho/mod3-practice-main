523603ea9eea4e92be07970e51f8bfc011605257
Fernando Diaz Alaminos <diaz.alaminos.fernando@gmail.com>
2023-10-23 19:30:35 +0200

    exercise 14 finished

--- src/js/exercises/exercise-14.js
+++ src/js/exercises/exercise-14.js
@@ -13,19 +13,28 @@ Muestra un array que solo incluya la pelicula mejor valorada de cada uno de esto
 const filterFilmBestActor = (films, actors) => {
     let arrayFilmBestActor = [];
 
-    for (let i = 0; i < actors.length; i++) {
+    // for (let i = 0; i < actors.length; i++) {
 
+    //     let bestActorMovie = films
+    //     .filter(film => film.actors.includes(actors[i]))
+    //     .reduce((maxRatFilm, film) => (maxRatFilm.rating > film.rating ? maxRatFilm : film));
+
+    //     arrayFilmBestActor.push(bestActorMovie);
+    // }
+
+
+    actors.forEach(actorName => {
         let bestActorMovie = films
-        .filter(film => film.actors.includes(actors[i]))
-        .reduce((maxRatFilm, film) => (maxRatFilm.rating > film.rating ? maxRatFilm : film));
+            .filter(film => film.actors.includes(actorName))
+            .reduce((maxRatFilm, film) => (maxRatFilm.rating > film.rating ? maxRatFilm : film));
 
         arrayFilmBestActor.push(bestActorMovie);
-    }
+    });
 
     return arrayFilmBestActor;
-    
+
 }
 
-let actors = ['Leonardo DiCaprio','Robert De Niro','Tom Hanks'];
+let actors = ['Leonardo DiCaprio', 'Robert De Niro', 'Tom Hanks'];
 
 showContent(14, filterFilmBestActor(movies, actors));
