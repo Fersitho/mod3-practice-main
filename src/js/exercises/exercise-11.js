4479a87701bf8e39f001ee4b33810bfde6313984
Fernando Diaz Alaminos <diaz.alaminos.fernando@gmail.com>
2023-10-25 17:31:17 +0200

    develop listo
    * Ejercicios 1-20 finalizados en Develop los agregamos en master (#1)
    
    * exercise 2 finished
    
    * exercise 3 finished + string.js generic functions
    
    * exercise 4 finished
    
    * exercise 5 finished
    
    * exercise 6 finished
    
    * exercise 7 finished
    
    * exercise 8 finished
    
    * exercise 10 y 11 finished
    
    * exercise 12 finished
    
    * exercise 13 finished
    
    * exercise 14 finished
    
    * exercise 14 finished
    
    * excrise 4 aclaraciones
    
    * exercise 15 finished
    
    * add-exercises.js async to load order asc
    
    * ejercicios 16 y 17 acabados
    
    * exercise 18 acabado
    
    * exercie 19 finished
    
    * exercise 20 finishedd
    
    * exce 20 fin
    
    * hotFix exercie 11 mayor a menor

--- src/js/exercises/exercise-11.js
+++ src/js/exercises/exercise-11.js
@@ -5,8 +5,8 @@ Ordena las peliculas por valoracion (propiedad "rating") de mayor a menor
 
 const orderFilmByRating = (movies) => {
 
-    return movies.sort((a,b) => a.rating - b.rating)
-
+    return movies.sort((a,b) => b.rating - a.rating)
+  
 }
 
 showContent(11, orderFilmByRating(movies));
